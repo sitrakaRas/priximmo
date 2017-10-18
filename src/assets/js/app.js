544d24ae43aa6b3397e002024156a8efeeccbb93
tolotra <tolotra.ramahatoraka@bocasay.fr>
2017-10-19 01:13:40 +0300

    .

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,31 +14,21 @@ import slick from 'slick-carousel';
 $(document).foundation();
 
 $('.slider').slick({
-  
-  // normal options...
   infinite: false,
-  
-  // the magic
   responsive: [{
-    
     breakpoint: 1024,
     settings: {
       slidesToShow: 3,
       infinite: true
     }
-    
   }, {
-    
     breakpoint: 600,
     settings: {
       slidesToShow: 2,
       dots: true
     }
-    
   }, {
-    
     breakpoint: 300,
     settings: "unslick" // destroys slick
-    
   }]
 });
