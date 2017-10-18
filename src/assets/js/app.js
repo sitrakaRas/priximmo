149686cce03bec12e46ef6b6613a9c3cf98a4a7b
tolotra <tolotra.ramahatoraka@bocasay.fr>
2017-10-18 22:15:28 +0300

    Boring Fontawesome, slick-carousel install

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,9 +4,41 @@ import whatInput from 'what-input';
 window.$ = $;
 
 import Foundation from 'foundation-sites';
+import slick from 'slick-carousel';
+
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
 
 
 $(document).foundation();
+
+$('.slider').slick({
+  
+  // normal options...
+  infinite: false,
+  
+  // the magic
+  responsive: [{
+    
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      infinite: true
+    }
+    
+  }, {
+    
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      dots: true
+    }
+    
+  }, {
+    
+    breakpoint: 300,
+    settings: "unslick" // destroys slick
+    
+  }]
+});
